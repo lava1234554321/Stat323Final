59a65dbb17e09ca752121a375db492bb4939f390
an102 <alicen2121@gmail.com>
2016-05-02 11:05:16 -0400

    yay preprocessing

--- sta323finalproject.R
+++ sta323finalproject.R
@@ -1,8 +1,8 @@
 library(jsonlite)
 library(dplyr)
 
-business <- stream_in(file('D:\\Yelp\\yelp_academic_dataset_business.json'))
-photos<-stream_in(file('D:\\YelpPhotos\\photo_id_to_business_id.json'))
+business <- stream_in(file('~/Yelp/yelp_academic_dataset_business.json'))
+photos<-stream_in(file('~/Yelp/photo_id_to_business_id.json'))
 
 business <- business[which(business$business_id %in% photos$business_id),]
 
@@ -39,8 +39,37 @@ business$Pop <- as.numeric(gsub(",","", business$Pop))
 int.var <- c('business_id','full_address','main_cat', 'city','review_count', 'name', 'longitude', 'latitude', 'state', 'stars', 'Zip','Mean','Median','Pop')
 business.main <- business[,which(names(business)%in%int.var)]
 colnames(business.main)<-paste(colnames(business)[which(names(business)%in% int.var)])
+Restaurant.hours <- business$hours
+lapply(Restaurant.hours$close,strptime,format='%H:%M')
+for(i in 1:ncol(Restaurant.hours)){
+  close<-strptime(Restaurant.hours[[i]]$close, format = '%H:%M')
+  open<- strptime(Restaurant.hours[[i]]$open, format = '%H:%M')
+  diff<- difftime(close,open,units = "hours")
+  needAdd <- which(diff<=0)
+  diff[needAdd] = diff[needAdd] + 24
+  diff[which(is.na(diff))] = 0
+  Restaurant.hours[[i]]$numHours = diff
+}
+Restaurant.hours$WeekdayHours = Restaurant.hours$Monday$numHours+Restaurant.hours$Tuesday$numHours+Restaurant.hours$Wednesday$numHours+Restaurant.hours$Thursday$numHours+Restaurant.hours$Friday$numHours
+Restaurant.hours$WeekendHours = Restaurant.hours$Saturday$numHours+Restaurant.hours$Sunday$numHours
 
-
-reviews<-stream_in(file('D:\\Yelp\\yelp_academic_dataset_review.json'))
+#attributes
+Restaurant.attributes <- cbind(business$attributes$`Accepts Credit Cards`,
+                               business$attributes$`Price Range`,
+                               business$attributes$Delivery,
+                               business$attributes$`Take-out`,
+                               business$attributes$`Takes Reservations`)
+ResAtNames <- c('CreditCard', 'Price', 'Delivery', 'Takeout', 'Reservations')
+Restaurant.att <- NA
+for(i in 1:5){
+  
+  Restaurant.attributes[unlist(lapply(Restaurant.attributes[,i],is.null)),i]<-NA
+  Restaurant.att<-cbind(Restaurant.att,unlist(Restaurant.attributes[,i]))
+}
+Restaurant.att<- Restaurant.att[,-1]
+colnames(Restaurant.att)<- paste(ResAtNames)
+Restaurant.att
+Restaurant.good<-res7$attributes$`Good For`
+reviews<-stream_in(file('~/Yelp/yelp_academic_dataset_review.json'))
 reviews<-reviews[which(reviews$business_id %in% business$business_id),]
 photos<-photos[which(photos$business_id %in% business$business_id),]
